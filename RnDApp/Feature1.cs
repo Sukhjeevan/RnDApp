0caadb1d7090ae7e2c9b0573eee59a85b3d573dd
Singh,Sukhjeevan <ssingh@nationallife.com>
2018-11-13 01:19:55 -0500

    Added Add method

--- RnDApp/Feature1.cs
+++ RnDApp/Feature1.cs
@@ -7,5 +7,12 @@ namespace RnDApp
 {
     public class Feature1
     {
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+            return sum;
+        }
     }
 }
